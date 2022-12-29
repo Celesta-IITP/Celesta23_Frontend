4704fe3d5fdee02aed6faceb22868159edd5029a
Divit <divitajmera15@gmail.com>
2022-12-30 00:41:34 +0530

    fixed iframe

--- src/components/ThemeVideo/ThemeVideo.js
+++ src/components/ThemeVideo/ThemeVideo.js
@@ -10,19 +10,20 @@ function ThemeVideo() {
 
         <img src={Video} className="header-video" alt="header-title"></img>
       </div>
+
       <div className="video-box">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/5NjFuS_24v8"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          className="video-element elem-size"
-        ></iframe>
         <div className="overlay overlay-1"></div>
         <div className="overlay overlay-2"></div>
         <div className="overlay overlay-3"></div>
         <div className="overlay overlay-4"></div>
         <div className="overlay overlay-5"></div>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/G3UDmUifQ9c"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          className="video-element elem-size"
+        />
       </div>
     </div>
   );
