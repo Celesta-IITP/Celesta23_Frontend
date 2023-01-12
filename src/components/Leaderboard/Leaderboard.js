1c0dcd8fa38678d58852ee6d277a4722a35acc13
Nishant Rao <nishantrao2003@gmail.com>
2023-01-13 02:48:22 +0530

    Added leaderboard

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -5,57 +5,106 @@ function Leaderboard(show) {
   const [data, setData] = useState([]);
   useEffect(() => {
     fetch(
-      "https://script.google.com/macros/s/AKfycbxSKOx6MYR-ofzQE2ufigtsLKUj67T0X2l2t60Hmxb2StDSpeZR9MqZtOQitjGp_iUpjw/exec"
+      "https://script.google.com/macros/s/AKfycby1yHmlQIfqI2fdWkfc0n-H1ujqekS6v5H9trWoX_TSYXsjC2A6kyJKLq8YPHHriWMHpw/exec"
     ).then((res) => {
       res.json().then((value) => {
         setData(value);
       });
     });
   }, []);
-  function DisplayData(data) {
-    data.map((row) => {
-      return (
-        <tr className="leader-row">
-          <td className="column-data">{row.name}</td>
-          <td className="column-data">{row.points}</td>
-          <td className="column-data">{row.id}</td>
-        </tr>
-      );
-    });
-  }
+  
   return (
-    <div className="leaderboard-container">
-      <h2 className="leader-header">Leaderboard</h2>
-      <div>
-        <table className="leader-table">
-          <thead>
-            <tr className="leader-row header-row">
+    <>
+      
+      <div className="LeaderTable">
+      <table className="leader-table">
+          <thead className="table-titles">
+            <tr className="header-row">
+              <th className="leader-column-header">Rank</th>
               <th className="leader-column-header">Name</th>
               <th className="leader-column-header">Points</th>
               <th className="leader-column-header">Celesta ID</th>
             </tr>
           </thead>
-          <tbody>
-            <tr className="leader-row">
-              <td className="column-data">some name</td>
-              <td className="column-data">some points</td>
-              <td className="column-data">some id</td>
+          <tbody className="table-data">
+            {/*<tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
             </tr>
             <tr className="leader-row">
-              <td className="column-data">some name</td>
-              <td className="column-data">some points</td>
-              <td className="column-data">some id</td>
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
             </tr>
             <tr className="leader-row">
-              <td className="column-data">some name</td>
-              <td className="column-data">some points</td>
-              <td className="column-data">some id</td>
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
             </tr>
+            <tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
+            </tr>
+            <tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
+            </tr>
+            <tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
+            </tr>
+            <tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
+            </tr>
+            <tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
+            </tr>
+            <tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
+            </tr>
+            <tr className="leader-row">
+              <td className="rank-data" >10</td>
+              <td className="name-data">Aman Shekhar Kumar Singh</td>
+              <td className="points-data">1428</td>
+              <td className="celestaid-data">CLST40180</td>
+  </tr>*/}
+            {data.map((row) => {
+      return (
+        <tr className="leader-row">
+          <td className="rank-data" >{data.indexOf(row)+1}</td>
+          <td className="name-data">{row.name}</td>
+          <td className="points-data">{row.Points}</td>
+          <td className="celestaid-data">{row.celestaId}</td>
+        </tr>
+      );
+    })}
           </tbody>
           {/* <tbody>{DisplayData(data)}</tbody> */}
         </table>
       </div>
-    </div>
+        
+      
+    </>
+      
   );
 }
 
